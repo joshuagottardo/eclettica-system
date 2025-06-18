f70a9ee892626a04b324a9c51c89d63410031597
Joshua Gottardo <joshuagottardo@MacBook-Air-di-Joshua.local>
2025-06-18 23:58:20 +0200

    removed useless folders

--- aaa/electron/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  // esponi funzioni sicure qui
-});
--- aaa/renderer/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('open-inserimento');
-  if (btn) {
-    btn.addEventListener('click', () => {
-      window.location.href = 'inserimento.html';
-    });
-  }
-});
